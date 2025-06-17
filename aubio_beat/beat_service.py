f217361a775b47b6116a649bd82d54b7d4ec1de5
AF <af.rollingwater@mail.de>
2025-06-17 23:46:38 +0200

    Korrektur 16

--- aubio_beat/beat_service.py
+++ aubio_beat/beat_service.py
@@ -7,7 +7,7 @@ import threading
 import yaml
 
 # 1. Setze log_level und fifo_path
-log_level = os.environ.get("LOG_LEVEL")
+log_level = os.environ.get("LOG_LEVEL").upper()
 if not log_level:
     raise ValueError("LOG_LEVEL is not set. Please check add-on config.")
 
